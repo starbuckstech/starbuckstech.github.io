f17676e48ffc876b35865df0df7b783e5bf58df8
Adam Starbuck <github@strb.uk>
2025-02-13 10:21:44 -0600

    Add key

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,13 @@ import App from './App.tsx';
 import './index.css';
 import { AptabaseProvider } from "@aptabase/react";
 
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AptabaseProvider appKey="<A-SH-3492479320>">
+    <AptabaseProvider 
+    appKey= "<A-SH-3492479320>"
+    options={{
+      host: "http://a.starbuckstech.com/"
+    }}>
       <App />
     </AptabaseProvider>
   </StrictMode>
