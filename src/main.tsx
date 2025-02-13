f52859f59aaaf4d38926c71b729431e5e13871c2
Adam Starbuck <github@strb.uk>
2025-02-13 09:50:19 -0600

    add analytics test

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { AptabaseProvider } from "@aptabase/react";
 
-createRoot(document.getElementById('root')!).render(
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AptabaseProvider appKey="<A-SH-3492479320>">
+      <App />
+    </AptabaseProvider>
   </StrictMode>
 );
-
-// adding a line to trigger a build AGAIN asdfaaaaaaa
